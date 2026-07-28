0078a5f794da65798ef17031d4f1f656cb2a8ddc
i5ma011 <i5ma01ecc@gmail.com>
2026-07-27 17:09:59 -0700

    postals.js

--- postals.js
+++ postals.js
@@ -1,36 +1,20 @@
-// =====================================
-// ER:LC POSTAL COORDINATES
-// =====================================
-// Format:
-// "Postal Number": [Y, X]
+const postals={
 
 
-const postals = {
+"603":{
+
+x:500,
+y:400
+
+},
 
 
-    // Example postals
-    // These coordinates will be replaced
-    // with your real ER:LC map locations
+"502":{
 
+x:700,
+y:500
 
-    "603": [530, 420],
-
-    "502": [610, 500],
-
-    "401": [720, 640],
-
-    "301": [450, 700],
-
-    "302": [480, 730],
-
-    "303": [520, 760],
-
-    "304": [560, 780],
-
-
-    // Add more like this:
-    //
-    // "POSTAL": [Y, X]
+}
 
 
 };
